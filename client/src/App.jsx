d45ded87eb83cf97bfe94a6a7e8b6b943ed2ce03
Muhammad Hashim Ali <hashimtrox@gmail.com>
2025-03-30 17:19:43 +0500

    Tutor Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import TutorSearch from './pages/student/TutorSearch';
 import MySessions from './pages/student/MySessions';
 import Wishlist from './pages/student/Wishlist';
 import Notifications from './pages/student/Notifications';
+import TutorProfile from './pages/tutor/Profile';
+import TutorDashboard from './pages/tutor/Dashboard';
+import TutorSessions from './pages/tutor/Sessions';
+import TutorEarnings from './pages/tutor/Earnings';
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
         <Route path="/student/sessions" element={<MySessions />} />
         <Route path="/student/wishlist" element={<Wishlist />} />
         <Route path="/student/notifications" element={<Notifications />} />
+        <Route path="/tutor/dashboard" element={<TutorDashboard />} />
+        <Route path="/tutor/profile" element={<TutorProfile />} />
+        <Route path="/tutor/sessions" element={<TutorSessions />} />
+        <Route path="/tutor/earnings" element={<TutorEarnings />} />
       </Routes>
     </Router>
   );
