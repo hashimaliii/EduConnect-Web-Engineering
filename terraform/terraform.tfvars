63d4d13ca09e32118935d47b1d1625b568ec1987
Muhammad Hashim Ali <hashimtrox@gmail.com>
2026-05-20 16:33:36 +0500

    VPC Network Implemented

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region           = "us-east-1"
+environment          = "educonnect"
+vpc_cidr             = "10.0.0.0/16"
+public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnet_cidrs = ["10.0.10.0/24", "10.0.11.0/24"]
---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
