1b42537e53a8d4c5e4150c70965686c469490a4e
Muhammad Hashim Ali <hashimtrox@gmail.com>
2026-05-21 03:27:07 +0500

    Update math.test.js

--- app/tests/unit/math.test.js
+++ app/tests/unit/math.test.js
@@ -1,5 +1,5 @@
 describe('Math Unit Tests', () => {
-  test('1. Addition works', () => expect(1 + 1).toBe(3));
+  test('1. Addition works', () => expect(1 + 1).toBe(2));
   test('2. Subtraction works', () => expect(5 - 2).toBe(3));
   test('3. Multiplication works', () => expect(3 * 3).toBe(9));
   test('4. Division works', () => expect(10 / 2).toBe(5));
